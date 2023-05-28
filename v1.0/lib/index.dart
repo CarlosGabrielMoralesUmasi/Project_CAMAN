27b091d0ab4fac7d73df1bf35f07ef6dda852eb2
Carlos G. MU <52215770+CarlosGabrielMoralesUmasi@users.noreply.github.com>
2023-05-28 15:05:10 -0500

    Add files via upload

---
+++ v1.0/lib/index.dart
@@ -0,0 +1,4 @@
+// Export pages
+export '/sign_up_page/sign_up_page_widget.dart' show SignUpPageWidget;
+export '/main_page/main_page_widget.dart' show MainPageWidget;
+export '/signin/signin_widget.dart' show SigninWidget;
